48f59e720a7d7728668650fb5922efd566e35663
Rahul <rahulsm.27@gmail.com>
2024-04-08 09:01:19 +0530

    Basic

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ner-using-bert",
+    version="0.0.1",
+    author="Rahul Mahajan",
+    author_email="rahulsm.27@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
